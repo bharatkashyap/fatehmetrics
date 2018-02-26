38fc0f2ee79801f32c6759c097d1ad1998493207
Bharat Kashyap <bharatkashyap@outlook.com>
2018-02-26 16:52:04 +0530

    Remove authorisation middleware for demonstration

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 // Create authorisatoin middleware to redirect if token not set
-router.use("/", function (req, res, next) {
+/* router.use("/", function (req, res, next) {
     if (req.path !== "/login" && !req.session.token) {
         return res.redirect("/login?returnUrl=" + encodeURIComponent("/app"));
     }
@@ -13,7 +13,7 @@ router.use("/", function (req, res, next) {
 // Make JWT token available to angular app
 router.get("/token", function (req, res) {
     res.send(req.session.token);
-});
+}); */
 
 // Serve angular app files from the '/app' route
 router.use("/", express.static("app"));
